1e0e59b21688714f745105c07e99cde9b1f0979a
Tomasz Tarczyński <ttarczynski@e-point.pl>
2019-05-08 15:44:34 +0200

    Ex. 8 CandidateListComponent test

--- src/app/candidate-list/candidate-list.component.spec.ts
+++ src/app/candidate-list/candidate-list.component.spec.ts
@@ -3,6 +3,8 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CandidateListComponent} from './candidate-list.component';
 import {TableModule} from "primeng/table";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {of} from "rxjs";
+import {By} from "@angular/platform-browser";
 
 describe('CandidateListComponent', () => {
   let component: CandidateListComponent;
@@ -28,4 +30,23 @@ describe('CandidateListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render candidates list', async(() => {
+    component.candidates$ = of([
+      {id: 1, firstName: 'Joe', lastName: 'Doe'},
+    ]);
+    fixture.detectChanges();
+
+    const tableEl = fixture.nativeElement.querySelector('.ui-table');
+    expect(tableEl).toBeTruthy();
+
+    const headerElements = fixture.debugElement.queryAll(By.css('th'));
+    expect(headerElements).toBeTruthy();
+    expect(headerElements.length).toBe(2);
+
+    const rawElements = fixture.debugElement.queryAll(By.css('tr'));
+    expect(rawElements.length).toBe(2);
+    expect(rawElements[1].nativeElement.innerText).toContain('Joe')
+    expect(rawElements[1].nativeElement.innerText).toContain('Doe')
+  }));
 });
